bce5d3c32c82a416a4fc1712a88e5be09f7d4627
@17151032 <u17151032@tuks.co.za>
2020-08-26 20:55:28 +0200

    learning IRL

---
+++ iServz/Back-End/API/iServzDB/dbo/Tables/ServiceProvider.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ServiceProvider] (
+    [ID]           INT            IDENTITY (1, 1) NOT NULL,
+    [BusinessName] NVARCHAR (50)  NULL,
+    [Decription]   NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_ServiceProvider] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+

bd4755473585b0d63ed5e577369b9b533fd3fd89
@17151032 <u17151032@tuks.co.za>
2020-08-26 19:04:07 +0200

    Database initialization

---
+++ iServz/Back-End/API/iServzDB/dbo/Tables/USER.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[USER] (
+    [ID]           INT            IDENTITY (1, 1) NOT NULL,
+    [Email]        NVARCHAR (50)  NULL,
+    [Name]         NVARCHAR (50)  NULL,
+    [Surname]      NVARCHAR (50)  NULL,
+    [PasswordHash] NVARCHAR (MAX) NULL,
+    [PasswordSalt] NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_USER] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+

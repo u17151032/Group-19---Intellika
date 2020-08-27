b9c4ad202acc711e52d8f065608035a147ae1204
Aubrey Dube <u17151032@tuks.co.za>
2020-08-27 21:08:30 +0200

    delete 02

--- iServz/Back-End/API/iServzDB/dbo/Tables/Admin_1.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Admin] (
-    [ID]     INT IDENTITY (1, 1) NOT NULL,
-    [UserID] INT NULL,
-    CONSTRAINT [PK_Admin] PRIMARY KEY CLUSTERED ([ID] ASC),
-    CONSTRAINT [FK_Admin_USER] FOREIGN KEY ([UserID]) REFERENCES [dbo].[USER] ([ID])
-);
-
--- iServz/Back-End/API/iServzDB/dbo/Tables/Consumer_1.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Consumer] (
-    [ID]     INT IDENTITY (1, 1) NOT NULL,
-    [UserID] INT NULL,
-    CONSTRAINT [PK_Consumer] PRIMARY KEY CLUSTERED ([ID] ASC),
-    CONSTRAINT [FK_Consumer_USER] FOREIGN KEY ([UserID]) REFERENCES [dbo].[USER] ([ID])
-);
-

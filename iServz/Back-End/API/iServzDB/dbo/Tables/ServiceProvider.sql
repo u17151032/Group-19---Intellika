66b02a12d24b2d0b5cab1252f4bf642d2962e20d
AbelPhintsi <50372298+AbelPhintsi@users.noreply.github.com>
2020-08-27 11:27:18 +0200

    added register tables

--- iServz/Back-End/API/iServzDB/dbo/Tables/ServiceProvider.sql
+++ iServz/Back-End/API/iServzDB/dbo/Tables/ServiceProvider.sql
@@ -1,11 +1,15 @@
 ﻿CREATE TABLE [dbo].[ServiceProvider] (
-    [ID]            INT            IDENTITY (1, 1) NOT NULL,
-    [BusinessName]  NVARCHAR (50)  NULL,
-    [Decription]    NVARCHAR (MAX) NULL,
-    [AverageRating] FLOAT (53)     NULL,
-    [OwnerName]     NVARCHAR (50)  NULL,
-    CONSTRAINT [PK_ServiceProvider] PRIMARY KEY CLUSTERED ([ID] ASC)
+    [ID]           INT           IDENTITY (1, 1) NOT NULL,
+    [BusinessName] NVARCHAR (50) NULL,
+    [Description]  NVARCHAR (50) NULL,
+    [UserID]       INT           NULL,
+    CONSTRAINT [PK_ServiceProvider] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK_ServiceProvider_USER] FOREIGN KEY ([UserID]) REFERENCES [dbo].[USER] ([ID])
 );
+
+
+
+
 
 
 
--- iServz/Back-End/API/iServzDB/dbo/Tables/USER.sql
+++ iServz/Back-End/API/iServzDB/dbo/Tables/USER.sql
@@ -1,10 +1,13 @@
 ﻿CREATE TABLE [dbo].[USER] (
-    [ID]           INT            IDENTITY (1, 1) NOT NULL,
-    [Email]        NVARCHAR (50)  NULL,
-    [Name]         NVARCHAR (50)  NULL,
-    [Surname]      NVARCHAR (50)  NULL,
-    [PasswordHash] NVARCHAR (MAX) NULL,
-    [PasswordSalt] NVARCHAR (MAX) NULL,
+    [ID]           INT           IDENTITY (1, 1) NOT NULL,
+    [Email]        NVARCHAR (50) NULL,
+    [Name]         NVARCHAR (50) NULL,
+    [Surname]      NVARCHAR (50) NULL,
+    [PasswordHash] TEXT          NULL,
+    [PasswordSalt] TEXT          NULL,
+    [Role]         TEXT          NULL,
     CONSTRAINT [PK_USER] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
+
+
 

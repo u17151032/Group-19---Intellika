dd4cb35feb68a9cd9d59ae036c20d212cc3ec424
@17151032 <u17151032@tuks.co.za>
2020-08-27 08:56:34 +0200

    SP attribute addition

--- iServz/Back-End/API/iServzDB/dbo/Tables/ServiceProvider.sql
+++ iServz/Back-End/API/iServzDB/dbo/Tables/ServiceProvider.sql
@@ -3,8 +3,11 @@
     [BusinessName]  NVARCHAR (50)  NULL,
     [Decription]    NVARCHAR (MAX) NULL,
     [AverageRating] FLOAT (53)     NULL,
+    [OwnerName]     NVARCHAR (50)  NULL,
     CONSTRAINT [PK_ServiceProvider] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
+
+
 
 
 

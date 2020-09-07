a7555e440cdce6ef159d6613dc502ea02f2b245c
@17151032 <u17151032@tuks.co.za>
2020-09-07 11:41:03 +0200

    commit stash

--- iServz v.2/iservzFrontEnd/src/app/routes.ts
+++ iServz v.2/iservzFrontEnd/src/app/routes.ts
@@ -1,6 +1,7 @@
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ServiceRequestsComponent } from './service-requests/service-requests.component';
+import { ServiceRequestsComponent } from './members/service-requests/service-requests.component';
 import { ServiceOrdersComponent } from './service-orders/service-orders.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AuthGuard } from './_guards/auth.guard';
@@ -12,7 +13,8 @@ export const appRoutes: Routes = [
    runGuardsAndResolvers: 'always',
    canActivate: [AuthGuard],
    children: [
-    { path: 'serviceRequests', component: ServiceRequestsComponent},
+    { path: 'serviceRequests', component: ServiceRequestsComponent},//changed from members
+    { path: 'serviceRequests/:id', component: MemberDetailComponent},
     { path: 'serviceOrders', component: ServiceOrdersComponent},
     { path: 'messages', component: MessagesComponent},
    ]
